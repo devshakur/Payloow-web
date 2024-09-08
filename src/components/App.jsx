925f930df109cc9a1670d16f58537f4df2883a1b
abdulshakur <devshakur23@gmail.com>
2024-09-08 12:18:31 +0100

    Initialized prgoject environment and installed necessary dependencies

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-500 flex justify-center items-center'>Welcome to Revo</h1>
+    </div>
+  )
+}
+
+export default App
